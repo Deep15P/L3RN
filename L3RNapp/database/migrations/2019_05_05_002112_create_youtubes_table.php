9695c1cd988eb31f41c79fcc56ac73d995ef5a10
Rhino <ryanharrisfl@gmail.com>
2019-05-04 20:29:23 -0400

    added fields for youtube

--- L3RNapp/database/migrations/2019_05_05_002112_create_youtubes_table.php
+++ L3RNapp/database/migrations/2019_05_05_002112_create_youtubes_table.php
@@ -15,6 +15,10 @@ class CreateYoutubesTable extends Migration
     {
         Schema::create('youtubes', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('resultKind');
+            $table->string('etag');
+            $table->string('videoKind');
+            $table->string('videoID');
             $table->timestamps();
         });
     }
